aeb6f4f46fda2d2d9bf77fa55b805908f4af9b43
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2020-01-09 10:49:24 +0530

    MainActivity.java

---
+++ Source_code/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA=0;
+    int scoreTeamB=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void addOneForTeamA(View v){
+        scoreTeamA+=1;
+        displayForTeamA(scoreTeamA);
+
+    }
+    public void addTwoForTeamA(View v){
+        scoreTeamA+=2;
+        displayForTeamA(scoreTeamA);
+
+    }
+    public void addThreeForTeamA(View v){
+        scoreTeamA+=3;
+        displayForTeamA(scoreTeamA);
+    }
+    public void addOneForTeamB(View v){
+        scoreTeamA+=1;
+        displayForTeamB(scoreTeamB);
+
+    }
+    public void addTwoForTeamB(View v){
+        scoreTeamA+=2;
+        displayForTeamB(scoreTeamB);
+
+    }
+    public void addThreeForTeamB(View v){
+        scoreTeamA+=3;
+        displayForTeamB(scoreTeamB);
+    }
+    public void resetSCore(View v){
+        scoreTeamA=0;
+        scoreTeamB=0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+
+
+}
